60adcd518025a3d25951903680d64cad98d812af
AndrewDSkinner <AndrewDaneSkinner@protonmail.com>
2025-11-02 22:07:35 -0500

    remove webmvc annotation

--- src/main/java/com/AndrewDSkinner/social_prototype_backend/config/WebConfig.java
+++ src/main/java/com/AndrewDSkinner/social_prototype_backend/config/WebConfig.java
@@ -1,17 +1,15 @@
 package com.AndrewDSkinner.social_prototype_backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Adjust this to your frontend's origin
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin")
                 .allowCredentials(true);
